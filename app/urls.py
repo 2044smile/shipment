5077d28f699aadf76dd3604c5175dc9efa32b33f
cs_lee <47213853+2044smile@users.noreply.github.com>
2024-03-29 15:46:16 +0900

    Fix: minor

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 router = routers.DefaultRouter()
-router.register(r'items', views.ItemViewSet)
-router.register(r'orders', views.OrderViewSet)
-router.register(r'deliveries', views.DeliveryViewSet)
+router.register(r'menu', views.ItemViewSet)
+router.register(r'order', views.OrderViewSet)
+router.register(r'delivery', views.DeliveryViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
