fad97094f4e8f88e300d4cc2515c7c721982be20
2044smile <2044smile@naver.com>
2024-08-13 16:44:59 +0900

    Feat: CELERY_BROKER_URL typing error

--- config/settings.py
+++ config/settings.py
@@ -214,7 +214,7 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 # CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 # CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
-CELERY_BROKER_URL="amqp://guest:guest@rabbitmq:5672/"
+CELERY_BROKER_URL="amqp://guest:guest@rabbitmq:5672//"
 # CELERY_RESULT_BACKEND = 'django-db'
 
 SWAGGER_SETTINGS = {
