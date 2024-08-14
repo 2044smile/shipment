ff38c63b99ef08de750acc555e40796cc598033f
2044smile <2044smile@naver.com>
2024-08-14 12:09:35 +0900

    Fix: change IntegerField to PrimaryKeyRelatedField

--- app/serializers.py
+++ app/serializers.py
@@ -3,9 +3,11 @@ from rest_framework import serializers
 
 
 class ItemSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Item
-        fields = ['name', 'description', 'price']
+        fields = ['user', 'name', 'description', 'price']
 
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
@@ -13,13 +15,12 @@ class ItemSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    order = serializers.IntegerField(read_only=True)
-    user = serializers.IntegerField(read_only=True)
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
     purchase_date = serializers.DateTimeField(source="created_at", read_only=True)
 
     class Meta:
         model = Order
-        fields = ['order', 'user', 'items', 'purchase_date']
+        fields = ['user', 'item', 'purchase_date']
 
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
@@ -31,7 +32,7 @@ class DeliverySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Delivery
-        fields = ['order', 'sender', 'receiver', 'address', 'status']
+        fields = ['order', 'sender', 'address', 'status']
 
     def create(self, validated_data):
         validated_data['sender'] = self.context['request'].user  # 토큰 사용자는 sender
