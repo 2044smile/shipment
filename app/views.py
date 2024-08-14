806533f9e53f71afb2bd73c47b7093beddd8f805
2044smile <2044smile@naver.com>
2024-08-14 17:41:55 +0900

    Feat: add @property to models.py so i can use field name

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import ItemSerializer, OrderSerializer, DeliverySerializer
 from .models import Item, Order, Delivery
 from app.tasks import process_delivery_task
+from config.permissions import CannotPurchaseOwnProductPermission
 
 
 class ItemViewSet(viewsets.ModelViewSet):
@@ -32,10 +33,10 @@ class DeliveryViewSet(viewsets.ModelViewSet):
     def departure(self, request, pk=None):
         instance = self.get_object()
         if instance.status == '2':
-            return JsonResponse({'message': f'{instance.user.kakao_id} 고객님의 상품은 이미 배송이 완료 되었습니다.'})
+            return JsonResponse({'message': f'{instance.receiver.kakao_id} 고객님의 상품은 이미 배송이 완료 되었습니다.'})
         instance.status = '1'
         instance.save()
 
-        process_delivery_task.s(delivery_id=instance.id).apply_async(countdown=300)
+        process_delivery_task.s(delivery_id=instance.id).apply_async(countdown=60)
 
-        return JsonResponse({'message': f'{instance.user.kakao_id} 고객님의 상품 {" ".join([i.name for i in instance.order.items.all()])} 배송이 출발하였습니다. the delivery service will arrive in 300 seconds'})
+        return JsonResponse({'message': f'{instance.receiver.kakao_id} 고객님의 상품 {instance.order.item.name} 배송이 출발하였습니다. the delivery service will arrive in 60 seconds'})
