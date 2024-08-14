5c92f204b4f983b24c4247370916f2f91b228c92
2044smile <2044smile@naver.com>
2024-08-14 17:42:16 +0900

    Fix: minor

--- app/serializers.py
+++ app/serializers.py
@@ -28,12 +28,14 @@ class OrderSerializer(serializers.ModelSerializer):
 
 
 class DeliverySerializer(serializers.ModelSerializer):
-    sender = serializers.IntegerField(read_only=True)
+    sender = serializers.IntegerField(read_only=True)  # read_only=True 설정한 이유는 토큰(request.user) 로 사용자 정보를 가져와서 sender로 저장하기 위함
 
     class Meta:
         model = Delivery
         fields = ['order', 'sender', 'address', 'status']
 
     def create(self, validated_data):
-        validated_data['sender'] = self.context['request'].user  # 토큰 사용자는 sender
-        return super().create(validated_data)
+        # validated_data 에 직접 추가하지 않고, 인스턴스를 생성할 때 sender를 지정
+        delivery = Delivery.objects.create(sender=self.context['request'].user, **validated_data)
+
+        return delivery
